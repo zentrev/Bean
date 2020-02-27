18911b8d0ab35767d88c22ed2634623911d19bb2
Spencer <southam.spencer.d@gmail.com>
2020-02-27 13:49:07 -0700

    created Beanstalk class

---
+++ Source/Bean/Beanstalk.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Beanstalk.h"
+
+// Sets default values
+ABeanstalk::ABeanstalk()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABeanstalk::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABeanstalk::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
