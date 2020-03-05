b1455d8b3aaacba21f9a6ce1c158f0098fe7d11c
Kali <c.t.raines.iii@gmail.com>
2020-03-05 10:10:24 -0700

    Add basic land and create new map

--- Source/Bean/BeanGameMode.cpp
+++ Source/Bean/BeanGameMode.cpp
@@ -7,7 +7,7 @@
 ABeanGameMode::ABeanGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_BeanCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
